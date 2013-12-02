dc730e57725fbed687e13e33f2e3a4baddafd05d
gaojiewang <gaojiewang@meituan.com>
2013-12-02 23:24:12 +0800

    初始化分层

---
+++ src/main/java/com/twoguys/bcstore/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.twoguys.bcstore.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gaojiewang
+ * Date: 13-12-2
+ * Time: 下午11:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserController {
+}
